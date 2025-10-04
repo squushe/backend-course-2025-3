1ca6b281e145eb52aa4cac51ef2b03c51693c3a4
squushe bc2025-3 <maksimrozko49@gmail.com>
2025-10-04 14:30:13 +0300

    Added parameters -s,-a corrected titanic.json and added output file

--- main.js
+++ main.js
@@ -3,7 +3,9 @@ const fs = require("fs");
 program
   .option("-i,--input <path>")
   .option("-o, --output <path>")
-  .option("-d,--display");
+  .option("-d,--display")
+  .option("-s,--survived")
+  .option("-a,--age");
 
 program.parse();
 
@@ -23,10 +25,30 @@ if (!options.output && !options.display) {
   process.exit(0);
 }
 
-let result = `Обробка файлу ${options.input}`;
+const fileContent = fs.readFileSync(options.input, "utf-8");
+const data = JSON.parse(fileContent);
+let workData = data;
+
+if (options.survived) {
+  workData = workData.filter((passanger) => passanger.Survived == "1");
+}
+
+const formattedLines = workData.map((passenger) => {
+  const parts = [passenger.Name];
+
+  if (options.age) {
+    parts.push(passenger.Age);
+  }
+
+  parts.push(passenger.Ticket);
+
+  return parts.join(" ");
+});
+const result = formattedLines.join("\n");
 
 if (options.output) {
   console.log("Результат записано у файл", options.output);
+  fs.writeFileSync(options.output, result);
 }
 
 if (options.display) {
