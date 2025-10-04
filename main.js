ad336605fea4bc6026bab32215125334dfde7583
squushe bc2025-3 <maksimrozko49@gmail.com>
2025-10-04 13:44:28 +0300

    Added parameters -i,-o,-d

--- main.js
+++ main.js
@@ -1,1 +1,35 @@
 const { program } = require("commander");
+const fs = require("fs");
+program
+  .option("-i,--input <path>")
+  .option("-o, --output <path>")
+  .option("-d,--display");
+
+program.parse();
+
+const options = program.opts();
+
+if (!options.input) {
+  console.error("Please, specify input file");
+  process.exit(1);
+}
+
+if (!fs.existsSync(options.input)) {
+  console.error("Cannot find input file");
+  process.exit(1);
+}
+
+if (!options.output && !options.display) {
+  process.exit(0);
+}
+
+let result = `Обробка файлу ${options.input}`;
+
+if (options.output) {
+  console.log("Результат записано у файл", options.output);
+}
+
+if (options.display) {
+  console.log("Результат в консоль");
+  console.log(result);
+}
